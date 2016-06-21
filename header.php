2dfd069e61ebbfb5476a7f9b792b6f58beac5cbf
Randy Klasen <rande-e@hotmail.com>
2016-06-21 21:34:21 +0200

    first push with files

---
+++ header.php
@@ -0,0 +1,71 @@
+<html>
+  <head>
+
+    <title>Header</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+    integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="inc/css/header.css"/>
+    <link type="text/css" rel="stylesheet" href="inc/css/slideme.css"/>
+
+    <script type="text/javascript" src="inc/js/jquery-2.1.4.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+    integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="inc/js/jquery.slideme2.js"></script>
+    <script type="text/javascript" src="inc/js/scroll.js"></script>
+
+    <script type="text/javascript" src="inc/js/default.js"></script>
+
+  </head>
+  <body>
+
+    <nav class="navbar navbar-default" role="navigation">
+      <div class="container">
+  <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+
+    <!-- Navbar dropdown button for mobile screens.-->
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-collapse" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="#" class="navbar-brand">
+          <img alt="BiKE" src="images/logo.png" class="img-responsive">
+        </a>
+        <p href="#" class="hidden-sm hidden-md hidden-lg">BiKE</p>
+
+      </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="nav-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About BiKE</a></li>
+          <li><a href="#">Functions</a></li>
+          <li><a href="#">Demo</a></li>
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+    </nav>
+    <div class="container-fluid">
+      <div id="sliderwrapper">
+        <ul class="slideme">
+          <li><img width="100%" src="images/bikeimg1.jpg"/></li>
+          <li><img width="100%" src="images/bikeimg2.jpg"/></li>
+          <li><img width="100%" src="images/bikeimg3.jpg"/></li>
+          <li><img width="100%" src="images/bikeimg4.jpg"/></li>
+        </ul>
+      </div>
+      <div class="col-md-12 holder">
+        <h1>BiKE</h1>
+        <p>
+          The bicycle you dream about.
+        </p>
+      </div>
+    </div>
+  </body>
+</html>
